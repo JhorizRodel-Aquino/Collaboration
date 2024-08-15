b740feeac833ab255d9602a67ee4ae69ec7459b9
Lornz18 <mellgodwinbarza22@gmail.com>
2024-08-15 15:53:13 +0800

    tna

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,32 @@
-var slider = tns({
-  container: ".testimonial__slider",
+var slider2 = tns({
+  container: ".services__slider",
   items: 3,
   speed: 200,
-  gutter: 100,
-  slideBy: "page",
-  autoplay: true,
-  navPosition: "bottom",
-  containerControls: "#controls",
-  prevButton: ".prev",
-  nextButton: ".next",
+  gutter: 20,
+  slideBy: "1",
+  rewind: true,
+  containerControls: "#services__btn",
+  prevButton: ".servicesPress__prev",
+  nextButton: ".servicesPress__next",
+  responsive: {
+    1250: {
+        items: 3,
+    },
+    200: {
+        items: 1,
+    },
+  },
+});
+var slider3 = tns({
+  container: ".services__slider2",
+  items: 3,
+  speed: 200,
+  gutter: 20,
+  slideBy: "1",
+  rewind: true,
+  containerControls: "#services__btn",
+  prevButton: ".servicesPress__prev",
+  nextButton: ".servicesPress__next",
   responsive: {
     1250: {
         items: 3,
