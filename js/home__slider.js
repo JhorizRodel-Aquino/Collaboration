f72005820778b7ffa5c139c01f8a75bfd46c6c7d
Lornz18 <mellgodwinbarza22@gmail.com>
2024-08-16 10:36:38 +0800

    bitlog

---
+++ js/home__slider.js
@@ -0,0 +1,24 @@
+var homeService2__sldr = tns({
+  container: ".homeService2__slider",
+  items: 3,
+  speed: 500,
+  gutter: 30,
+  slideBy: "1",
+  containerControls: "#homeService2__ctrls",
+  prevButton: ".homeService2__prev",
+  nextButton: ".homeService2__next",
+  rewind: true,
+});
+
+var homeBanner__sldr = tns({
+  container: ".homeBanner__slider",
+  items: 1,
+  speed: 500,
+  gutter: 0,
+  slideBy: "1",
+  axis: "vertical",
+  controls: false,
+  autoplay: true,
+});
+
+
--- js/services__slider.js
+++ js/services__slider.js
@@ -1,24 +1,5 @@
-var slider2 = tns({
+var slider1 = tns({
   container: ".services__slider",
-  items: 3,
-  speed: 200,
-  gutter: 20,
-  slideBy: "1",
-  rewind: true,
-  containerControls: "#services__btn",
-  prevButton: ".servicesPress__prev",
-  nextButton: ".servicesPress__next",
-  responsive: {
-    1250: {
-        items: 3,
-    },
-    200: {
-        items: 1,
-    },
-  },
-});
-var slider3 = tns({
-  container: ".services__slider2",
   items: 3,
   speed: 200,
   gutter: 20,
@@ -36,25 +17,23 @@ var slider3 = tns({
     },
   },
 });
-var homeService2__sldr = tns({
-  container: ".homeService2__slider",
-  items: 3,
-  speed: 500,
-  gutter: 30,
-  slideBy: "1",
-  containerControls: "#homeService2__ctrls",
-  prevButton: ".homeService2__prev",
-  nextButton: ".homeService2__next",
-  rewind: true,
-});
 
-var homeBanner__sldr = tns({
-  container: ".homeBanner__slider",
-  items: 1,
-  speed: 500,
-  gutter: 0,
+var slider2 = tns({
+  container: ".services__slider2",
+  items: 3,
+  speed: 200,
+  gutter: 20,
   slideBy: "1",
-  axis: "vertical",
-  controls: false,
-  autoplay: true,
+  rewind: true,
+  containerControls: "#services__btn",
+  prevButton: ".servicesPress__prev",
+  nextButton: ".servicesPress__next",
+  responsive: {
+    1250: {
+      items: 3,
+    },
+    200: {
+      items: 1,
+    },
+  },
 });
